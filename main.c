88905e46ddca62a45d216a9c60ac264e093da3de
tjrwldnjs <sue06024@naver.com>
2018-11-22 15:24:08 +0900

    today2

--- main.c
+++ main.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	char src[100]="Programming course";
-	char dst[100];
 	int i=0;
 	
 	while( src[i] != '\0')
 	{
-		dst[i] = src[i];
 		i++;
 	}
-	dst[i]='\0'; //반드시 넣어야함. 안넣으면 프로그램 뻗을 수 있음. 
+	printf("%s (%i)\n", src, i);
+	printf("%i", strlen(src));
 	
-	
-	printf("dst : %s\n",dst);
+	//strcpy(dst, src);
 	return 0;
 }
